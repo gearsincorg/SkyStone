68962aa8c8a73cac62c49ae86d0cd45208aa717e
GearsIncOrg <phil.malone@mr-phil.com>
2019-10-06 11:55:49 -0400

    Create ServoTool.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTool.java
@@ -0,0 +1,107 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * This OpMode scans a single servo back and forwards until Stop is pressed.
+ * The code is structured as a LinearOpMode
+ * INCREMENT sets how much to increase/decrease the servo position each cycle
+ * CYCLE_MS sets the update period.
+ *
+ * This code assumes a Servo configured with the name "left_hand" as is found on a pushbot.
+ *
+ * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
+ * connected servos are able to move freely before running this test.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+@TeleOp(name = "ServoTool", group = "Concept")
+@Disabled
+public class ServoTool extends LinearOpMode {
+
+    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS    =   50;     // period of each cycle
+    static final double MAX_POS     =  1.0;     // Maximum rotational position
+    static final double MIN_POS     =  0.0;     // Minimum rotational position
+
+    // Define class members
+    Servo   servo1;
+    Servo   servo2;
+    double  pos1 = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double  pos2 = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+
+    @Override
+    public void runOpMode() {
+
+        // Connect to servo (Assume PushBot Left Hand)
+        // Change the text in quotes to match any servo name on your robot.
+        servo1 = hardwareMap.get(Servo.class, "s1");
+        servo2 = hardwareMap.get(Servo.class, "s2");
+
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
+
+            if (gamepad1.y && (pos1 < MAX_POS))    {
+                pos1 += 0.01;
+            }
+            else if (gamepad1.a && (pos1 > MIN_POS)) {
+                pos1 -= 0.01;
+            }
+
+            if (gamepad1.x && (pos2 < MAX_POS))    {
+                pos2 += 0.01;
+            }
+            else if (gamepad1.b && (pos2 > MIN_POS)) {
+                pos2 -= 0.01;
+            }
+
+            servo1.setPosition(pos1);
+            servo2.setPosition(pos2);
+
+            // Display the current values
+            telemetry.addData(">", "Y-A Servo 1, X-B Servo 2" );
+            telemetry.addData("Servo1", "%5.2f", pos1);
+            telemetry.addData("Servo2", "%5.2f", pos2);
+            telemetry.update();
+
+            sleep(CYCLE_MS);
+        }
+
+        // Signal done;
+        telemetry.addData(">", "Done");
+        telemetry.update();
+    }
+}
